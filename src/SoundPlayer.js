b0198644a83bf496fc79130c3689573f60b885b1
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-08-09 21:31:24 +0200

    some changes

--- src/SoundPlayer.js
+++ src/SoundPlayer.js
@@ -86,6 +86,12 @@ class SoundPlayer extends EventEmitter {
          */
         this.playbackRate = 1;
 
+        /**
+         * pm: An additional delay (in seconds) to transition volume on sound stop.
+         * @type {number}
+         */
+        this.stopFadeDecay = 0;
+
         // handleEvent is a EventTarget api for the DOM, however the
         // web-audio-test-api we use uses an addEventListener that isn't
         // compatable with object and requires us to pass this bound function
@@ -250,7 +256,7 @@ class SoundPlayer extends EventEmitter {
      * If the sound is already playing it will stop playback with a quick fade
      * out.
      */
-    play () {
+    play (startSeconds) {
         if (this.isStarting) {
             this.emit('stop');
             this.emit('play');
@@ -267,12 +273,16 @@ class SoundPlayer extends EventEmitter {
             this.initialize();
         }
 
-        this.outputNode.start();
+        if (typeof startSeconds === "number") {
+            this.outputNode.start(0, startSeconds);
+        } else {
+            this.outputNode.start();
+        }
 
         this.isPlaying = true;
 
         const {currentTime, DECAY_DURATION} = this.audioEngine;
-        this.startingUntil = currentTime + DECAY_DURATION;
+        this.startingUntil = currentTime + (DECAY_DURATION + this.stopFadeDecay);
 
         this.emit('play');
     }
@@ -298,9 +308,9 @@ class SoundPlayer extends EventEmitter {
 
         taken.finished().then(() => taken.dispose());
 
-        taken.volumeEffect.set(0);
+        taken.volumeEffect.set(0, this.stopFadeDecay);
         const {currentTime, DECAY_DURATION} = this.audioEngine;
-        taken.outputNode.stop(currentTime + DECAY_DURATION);
+        taken.outputNode.stop(currentTime + (DECAY_DURATION + this.stopFadeDecay));
     }
 
     /**
